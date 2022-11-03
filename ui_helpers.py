8fdb1de1b2e0a2a0c3a6ce8e67f392968699a77c
Louis Li <louisrli@gmail.com>
2022-11-03 17:25:36 +0300

    implement serialization of the config

--- ui_helpers.py
+++ ui_helpers.py
@@ -1,14 +1,5 @@
 from aqt.qt import *
 
-COLUMN_LABELS = [
-    "Label",
-    "Search Query",
-    "Target Field",
-    "Result Count",
-    "If not empty?",
-    "",
-    ""]
-
 class OverwriteValues:
     """
     Possible values for the overwrite config.
@@ -32,6 +23,22 @@ class ConfigKeys:
     HEIGHT = "height"
     OVERWRITE = "overwrite"
 
+# The order in which the keys appear as columns in the query config form.
+#
+# This is an extremely unideal way to do this (hacky) but basing on the legacy
+# code: to extract the values from the form later, we need to know what order
+# the columns appear.
+# TODO: Add some assertions to check this. 
+QUERY_CONFIG_KEY_ORDER = (
+    ConfigKeys.LABEL,
+    ConfigKeys.SEARCH_TERM,
+    ConfigKeys.TARGET_FIELD,
+    ConfigKeys.RESULT_COUNT,
+    ConfigKeys.OVERWRITE,
+    ConfigKeys.WIDTH,
+    ConfigKeys.HEIGHT)
+        
+
 class ConfigDefaults:
     RESULT_COUNT = 1
     WIDTH = -1
@@ -41,6 +48,14 @@ class ConfigDefaults:
     # The placeholder value in the search term the user provides.
     WORD_PLACEHOLDER = "{}"
 
+COLUMN_LABELS = [
+    "Label",
+    "Search Query",
+    "Target Field",
+    "Result Count",
+    "If not empty?",
+    "",
+    ""]
 
 def make_target_field_select(options, config_value) -> QComboBox:
     """
@@ -74,6 +89,7 @@ def make_overwrite_select(config_value) -> QComboBox:
     select.addItem(OverwriteValues.SKIP)
     select.addItem(OverwriteValues.OVERWRITE)
     select.addItem(OverwriteValues.APPEND)
+    # TODO: Need to match the values correctly here.
     select.setCurrentIndex(select.findText(config_value))
     return select
 
@@ -101,4 +117,32 @@ def serialize_config_from_ui(form):
 
     See config.json for the default config.
     """
-    pass
+    source_field = form.sourceField.currentText()
+    config[ConfigKeys.SOURCE_FIELD] = source_field
+
+    query_configs = []
+    num_ui_columns = form.gridLayout.columnCount()
+    assert(num_ui_columns == len(QUERY_CONFIG_KEY_ORDER))
+    for i in range(1, form.gridLayout.rowCount()):
+        q = {}
+        # Well, in general this approach is highly coupled with the UI, but I'm
+        # too lazy to change it for now, so I'm copying this guy's code.
+        for j in range(form.gridLayout.columnCount()):
+            key = QUERY_CONFIG_KEY_ORDER[j]
+            item = form.gridLayout.itemAtPosition(i, j)
+
+            if isinstance(item, QWidgetItem):
+                item = item.widget()
+            elif isinstance(item, QLayoutItem):
+                item = item.itemAt(1).widget()
+            if isinstance(item, QComboBox):
+                # Overwrite and target field boxes
+                q[key] = item.currentText()
+            elif isinstance(item, QSpinBox):
+                q[key] = item.value()
+            else:
+                q[key] = item.text()
+        query_configs.append(q)
+    config[ConfigKeys.QUERY_CONFIGS] = query_configs
+    return config
+
